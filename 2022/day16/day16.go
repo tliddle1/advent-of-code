e81e1619b2f5c470bfc0b267c6a1e9cb61ef8a38
Thomas Liddle <thomas@smarty.com>
2023-09-23 18:07:52 -0600

    boilerplate

---
+++ 2022/day16/day16.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+)
+
+func main() {
+	day := 16
+	fmt.Println(part1(day), part2(day))
+}
+
+func part1(day int) int {
+	filename := fmt.Sprintf("day%d/day%d.txt", day, day)
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return 0
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	/////////////////////////////
+	/////////////////////////////
+	for {
+		line, err := reader.ReadString('\n')
+		line = trimSpace(line)
+		/////////////////////////////
+		/////////////////////////////
+		if err != nil {
+			break
+		} //EOF
+	}
+
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	/////////////////////////////
+	return 0
+	/////////////////////////////
+}
+
+func part2(day int) int {
+	filename := fmt.Sprintf("day%d/day%d.txt", day, day)
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return 0
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	/////////////////////////////
+	/////////////////////////////
+	for {
+		line, err := reader.ReadString('\n')
+		line = trimSpace(line)
+		/////////////////////////////
+		/////////////////////////////
+		if err != nil {
+			break
+		} //EOF
+	}
+
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	/////////////////////////////
+	return 0
+	/////////////////////////////
+}
+
+func trimSpace(s string) string {
+	return string(bytes.TrimSpace([]byte(s)))
+}
