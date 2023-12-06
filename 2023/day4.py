b77f59113dfd6639f766c47eddc930e0df0f067f
Thomas Liddle <thomasliddle34@gmail.com>
2023-12-06 04:35:49 -0700

    Update day4.py part 1

--- 2023/day4.py
+++ 2023/day4.py
@@ -1,8 +1,24 @@
-x = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+inp = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
 Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
 Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
 
-sum([(2**(len(set(l.split(" | ")[0].split(": ")[1].split()).intersection(set(l.split(" | ")[1].split()))))//2) for l in x.split("\n")])
+def calc(line):
+    cards = line.split(" | ")
+    winning = set(cards[0].split(": ")[1].split())
+    mine = set(cards[1].split())
+
+    return 2**(len(winning.intersection(mine))) // 2
+def solve(lines):
+    result = 0
+    for line in lines:
+        result += calc(line)
+    return result
+
+lines = inp.split("\n")
+print(solve(lines))
+
+# One liner
+print(sum([(2**(len(set(l.split(" | ")[0].split(": ")[1].split()).intersection(set(l.split(" | ")[1].split()))))//2) for l in inp.split("\n")]))
