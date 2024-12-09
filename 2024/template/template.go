f90f3d5132a299d1eb5151ec3200fa0b2571ed3b
Thomas Liddle <thomas@smarty.com>
2024-12-08 21:45:04 -0700

    Update template

--- 2024/template/template.go
+++ 2024/template/template.go
@@ -6,23 +6,36 @@ import (
 
 func part1(filePath string) int {
 	input := parse.GetInput(filePath)
-	return solve(input)
+	return solve1(input)
 }
 
-func solve(input []string) int {
+func solve1(input []string) int {
 	var result int
 	for i, line := range input {
 		_ = i
-		result += calculate(line)
+		result += calculate1(line)
 	}
 	return result
 }
 
-func calculate(line string) int {
+func calculate1(line string) int {
 	return parse.StringToInt(line)
 }
 
 func part2(filePath string) int {
 	input := parse.GetInput(filePath)
-	return solve(input)
+	return solve2(input)
+}
+
+func solve2(input []string) int {
+	var result int
+	for i, line := range input {
+		_ = i
+		result += calculate2(line)
+	}
+	return result
+}
+
+func calculate2(line string) int {
+	return parse.StringToInt(line)
 }
